bff175edbdfbdd552f7acd65af64015750a9cfde
armok <84870462+72armok@users.noreply.github.com>
2021-06-08 00:17:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from PIL import Image
 import discord
+from discord import file
 from discord.embeds import Embed
 from discord.ext import commands
 import random
 import math
 import requests
+import ctypes
+
+ctypes.windll.kernel32.SetConsoleTitleW("Distort-O-Bot")
 
 def rev(pixel):
     return math.ceil(math.sin(pixel / 100) * 100 + math.sin(pixel) * 100)
+
+def prev(pixel):
+    return math.ceil(math.tan(pixel / 100) * 100 + math.tan(pixel) * 100)
+
+def aprev(pixel):
+    return 256 - math.ceil(math.tan(pixel / 100) * 100 + math.tan(pixel) * 100)
 
 def generate(provider:str = "picsum.photos"):
     firstno = str(random.randint(100,1200))
@@ -24,6 +34,52 @@ def generate(provider:str = "picsum.photos"):
     myimg_supreme = myimg.point(rev)
     myimg_supreme.save("myimg.jpg")
 
+def pgenerate(provider:str = "picsum.photos"):
+    firstno = str(random.randint(100,1200))
+    secondno = str(random.randint(100,1200))
+    if int(firstno) < int(secondno):
+        while(int(firstno) < int(secondno)):
+            firstno = str(random.randint(100,1200))
+            secondno = str(random.randint(100,1200))
+    crt = open("008.jpg", "wb")
+    content = requests.get(f"https://{provider}/{firstno}/{secondno}").content
+    crt.write(content)
+    crt.close()
+    myimg = Image.open("008.jpg")
+    myimg_supreme = myimg.point(prev)
+    myimg_supreme.save("myimg.jpg")
+
+def apgenerate(provider:str = "picsum.photos"):
+    firstno = str(random.randint(100,1200))
+    secondno = str(random.randint(100,1200))
+    if int(firstno) < int(secondno):
+        while(int(firstno) < int(secondno)):
+            firstno = str(random.randint(100,1200))
+            secondno = str(random.randint(100,1200))
+    crt = open("008.jpg", "wb")
+    content = requests.get(f"https://{provider}/{firstno}/{secondno}").content
+    crt.write(content)
+    crt.close()
+    myimg = Image.open("008.jpg")
+    myimg_supreme = myimg.point(aprev)
+    myimg_supreme.save("myimg.jpg")
+
+def apgenimg(urls:str):
+    crt = open("008.jpg", "wb")
+    crt.write(requests.get(urls).content)
+    crt.close()
+    myimg = Image.open("008.jpg").convert('RGB')
+    myimg_supreme = myimg.point(aprev)
+    myimg_supreme.save("myimg.jpg")
+
+def pgenimg(urls:str):
+    crt = open("008.jpg", "wb")
+    crt.write(requests.get(urls).content)
+    crt.close()
+    myimg = Image.open("008.jpg").convert('RGB')
+    myimg_supreme = myimg.point(prev)
+    myimg_supreme.save("myimg.jpg")
+
 def genimg(urls:str):
     crt = open("008.jpg", "wb")
     crt.write(requests.get(urls).content)
@@ -37,16 +93,21 @@ bot = commands.Bot(command_prefix='dstrt@', help_command=None)
 @bot.event
 async def on_ready():
     print("ONLINE")
+    print(", ".join([i.name for i in bot.guilds]))
     await bot.change_presence(activity=discord.Game(name="Corrupted ROMs"))
+
+@bot.event
+async def on_guild_join():
+    print(", ".join([i.name for i in bot.guilds]))
 
 @bot.command()
 async def gen(msg):
     generate()
+    await msg.channel.send(file=discord.File('myimg.jpg'))
     try:   
         await bot.user.edit(avatar=open('myimg.jpg', 'rb').read())
     except:
         pass
-    await msg.channel.send(file=discord.File('myimg.jpg'))
 
 @bot.command()
 async def gens(ctx):
@@ -70,14 +131,66 @@ async def genrepeat(ctx, arg):
         await ctx.channel.send("Something went wrong, please check your arguments.")
 
 @bot.command()
-async def sunshineandkittens(ctx):
+async def cat(ctx):
     generate("placekitten.com")
     await ctx.channel.send(file=discord.File('myimg.jpg'))
 
 @bot.command()
 async def help(ctx):
     em = discord.Embed(title="**Booklet-In-One**")
-    em.add_field(name="Bot Commands", value="**dstrt@gen** : Generates a random image then corrupts it\n**dstrt@gens [ATTACH IMAGE]** : Corrupts a given image\n**dstrt@genrepeat [Repeat time]** : Distorts an image a repeated amount of time.\n**dstrt@sunshineandkittens** : Gives a corrupted image of a kitten\n")
+    em.add_field(name="Bot Commands (1)", value="**dstrt@gen** : Generates a random image then corrupts it\n**dstrt@gens [ATTACH IMAGE]** : Corrupts a given image\n**dstrt@genrepeat [Repeat time]** : Distorts an image a repeated amount of time.\n**dstrt@cat** : Gives a corrupted image of a kitten\n**dstrt@mono** : Generates a random corrupted image in black and white.\n**dstrt@monoo** : Ditto, but with a couple of changes.\n**dstrt@invite** : Gives you an invite link to invite Distort-O-Bot wherever you want.\n")
+    em.add_field(name="Bot Commands (2)", value="**dstrt@pgen** Generates a random image then p-corrupts it: \n**dstrt@pgens** : p-Corrupts a given image\n**dstrt@pcat** : Gives a p-corrupted image of a kitten\n**dstrt@apgen** : Generates a random image then ap-corrupts it\n**dstrt@apgens** : ap-Corrupts a given image\n**dstrt@apcat** : Gives an ap-corrupted image of a kitten\n")
     await ctx.channel.send(embed=em)
+
+@bot.command()
+async def invite(ctx):
+    em=discord.Embed(title="Invite Me To Other Servers!", url="https://discord.com/oauth2/authorize?client_id=851212703057444874&permissions=2147601408&scope=bot", description="It would be great for you to do so.", color=0x2d6476)
+    await ctx.send(embed=em)
+
+@bot.command()
+async def mono(ctx):
+    generate()
+    i_f = Image.open('myimg.jpg')
+    i_f = i_f.convert('1')
+    i_f.save('myimg.jpg')
+    await ctx.channel.send(file=discord.File('myimg.jpg'))
+
+@bot.command()
+async def monoo(ctx):
+    generate()
+    i_f = Image.open('myimg.jpg')
+    i_f = i_f.convert('L')
+    i_f.save('myimg.jpg')
+    await ctx.channel.send(file=discord.File('myimg.jpg'))
+
+@bot.command()
+async def pgen(ctx):
+    pgenerate()
+    await ctx.channel.send(file=discord.File('myimg.jpg'))
+
+@bot.command()
+async def pgens(ctx):
+    pgenimg(ctx.message.attachments[0].url)
+    await ctx.channel.send(file=discord.File('myimg.jpg'))
+
+@bot.command()
+async def pcat(ctx):
+    pgenerate("placekitten.com")
+    await ctx.channel.send(file=discord.File('myimg.jpg'))
+
+@bot.command()
+async def apgen(ctx):
+    apgenerate()
+    await ctx.channel.send(file=discord.File('myimg.jpg'))
+
+@bot.command()
+async def apgens(ctx):
+    apgenimg(ctx.message.attachments[0].url)
+    await ctx.channel.send(file=discord.File('myimg.jpg'))
+
+@bot.command()
+async def apcat(ctx):
+    apgenerate("placekitten.com")
+    await ctx.channel.send(file=discord.File('myimg.jpg'))
 
 bot.run(open("token.txt", "r").read())
